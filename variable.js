cc96c1e88a5026cc50a240f0e2b650df02da448c
pavanh <pavanhdsce@gmail.com>
2023-06-20 21:30:24 +0530

    info about variable declaration

--- variable.js
+++ variable.js
@@ -17,8 +17,37 @@
 */
 
 let message1, message2, message3;
-message1 = "this is string variable"
-var variable = "abcd"
-const USERID =  "USER_1234"
+message1 = "this is string variable";
+var variable = "abcd";
+const USERID = "USER_1234";
 
+//NOTE -HOISTING
+// * It refers to the behavior of moving "variable" and only "function declarations" to the top of their containing scope during the compilation phase, before the code is executed.
+
+console.log(x); // undefined
+var x = 5;
+console.log(y); //  ReferenceError: y is not defined
+
+funCall(); // this function is calling
+function funCall() {
+  console.log("this function is calling!");
+}
+
+
+//IMPORTANT
+// * scoping
+
+var globalVariable = "I'm a global variable"; // Global variable
+
+function myFunction() {
+  const val = 100; //block scoped
+  var localVariable = "I'm a local variable"; // Local variable
+  let localVariable = "I'm a local variable"; // function variable
+  console.log(localVariable);
+  console.log(globalVariable);
+}
+
+myFunction(); // Call the function
+console.log(globalVariable);
+console.log(localVariable); // ReferenceError: localVariable is not defined
 
