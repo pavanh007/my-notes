d754a7ab2df133a12604dea40d88e26a4b7b6b38
pavanh <pavanhdsce@gmail.com>
2023-06-20 21:08:37 +0530

    defining the variable

---
+++ variable.js
@@ -0,0 +1,24 @@
+/*
+// * named storage for storing the data
+// * variables are immutable
+// * Define the variable : let, var and const 
+// * scoping :
+            * var in function scooped 
+            * let is block scoped
+            * const is block scoped 
+// * let - define the variable and assign it later.
+// * const -  used to assign the constant variable
+// ! variable should not start with number(digit)
+// * variable declaration anology:
+    * variable name can start with "character", "$" and "_"
+    * variable names are case-sensitive
+    * variable names should not be latin
+    * If we use the "use strict", we can define variable with "let" 
+*/
+
+let message1, message2, message3;
+message1 = "this is string variable"
+var variable = "abcd"
+const USERID =  "USER_1234"
+
+
