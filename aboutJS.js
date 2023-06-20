06ed2cdbc217338c286e43f0be0950523ecf1c37
pavanh <pavanhdsce@gmail.com>
2023-06-20 21:34:48 +0530

    updated aboutJS

--- aboutJS.js
+++ aboutJS.js
@@ -1,8 +1,9 @@
 /*
 // * About JS
-// * 1. lightweight, 2. interpretened, 3. I/O operations, 4. JIT compiler, 5.single threaded
+// * 1. lightweight, 2. interpretened, 3. I/O operations, 4. JIT compiler, 5.single threaded, 5.dynamic language, 
 // * JIT(just-in-compiler), is an inventory management method in which goods are received from suppliers only as they are needed.
 // * It's used in both frontend framework like react, Vue, etc.. as we'll as backend using NodeJS.
+// * 
 */
 
 console.log("hello world");
